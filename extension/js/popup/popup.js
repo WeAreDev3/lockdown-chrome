cae6df81a43ed64321a6d7660abe3d29188123c7
Jesse Smick <janka102@gmail.com>
2014-06-26 18:49:22 -0700

    UI stuff, but now we decided to not use chrome-bootstrap

--- extension/js/popup/popup.js
+++ extension/js/popup/popup.js
@@ -1,1 +1,10 @@
-console.log('I\'m in the popup!');
+window.addEventListener('DOMContentLoaded', function () {
+    var inputs = document.getElementsByTagName('input');
+    for (var i = inputs.length - 1; i >= 0; i--) {
+        inputs[i].addEventListener('input', updateLabel);
+    }
+});
+
+function updateLabel () {
+    this.previousElementSibling.classList[(this.value === "" ? 'remove' : 'add')]('shown');
+}

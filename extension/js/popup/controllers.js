5de0e91ac5e269d8b9ba6250b207a6305f97658b
Jesse Smick <janka102@gmail.com>
2014-07-02 11:15:13 -0700

    Got client hashing working and sending over the data

--- extension/js/popup/controllers.js
+++ extension/js/popup/controllers.js
@@ -1,7 +1,7 @@
 angular.module('ld.navbar', [])
     .controller('NavbarRoute', ['$scope', '$location', '$log',
-        function(self, loc, log) {
-            self.isAt = function(where) {
+        function(scope, loc, log) {
+            scope.isAt = function(where) {
                 log.log(where, loc.path());
                 return where === loc.path();
             };
@@ -9,36 +9,63 @@ angular.module('ld.navbar', [])
     ]);
 
 angular.module('ld.main', [])
-    .controller('SignInController', ['$scope', '$log',
-        function(self, log) {
-            self.username = "";
-            self.password = "";
+    .value('baseUrl', 'http://localhost:3000')
+    .controller('SignInController', ['$scope', '$log', 'baseUrl',
+        function(scope, log) {
+            scope.username = "";
+            scope.password = "";
 
             var getCreds = function(username) {
                 http({
                     method: 'GET',
-                    url: '/signin',
+                    url: scope.baseUrl + '/signin',
                     params: {
                         username: username
                     }
                 })
                     .success(function(data, status, headers, config) {
-                        log.log(data);
+                        log.log(arguments);
                     })
                     .error(function(data, status, headers, config) {
-                        log.log(data);
+                        log.log(arguments);
                     });
             };
         }
     ])
-    .controller('SignUpController', ['$scope', '$http', '$log',
-        function(self, http, log) {
-            self.email = "";
-            self.username = "";
-            self.password = "";
+    .controller('SignUpController', ['$scope', '$http', '$log', 'baseUrl',
+        function(scope, http, log, baseUrl) {
+            scope.email = "";
+            scope.username = "";
+            scope.password = "";
 
-            self.submit = function() {
-                // Ecrypt and http POST to /users
+            scope.submit = function() {
+                // 32 bits = 4 bytes, 8 * 4 bytes = 32 byte long salt
+                var salt = new Int32Array(8);
+                window.crypto.getRandomValues(salt);
+
+                var pbkdf2Opts = {
+                    email: scope.email,
+                    username: scope.username,
+                    salt: sjcl.codec.base64.fromBits(salt),
+                    iter: 1000,
+                    keyLength: 512,
+                    hash: null,
+                };
+
+                var hash = sjcl.misc.pbkdf2(scope.password, pbkdf2Opts.salt, pbkdf2Opts.itr, pbkdf2Opts.keyLength);
+                pbkdf2Opts.hash = sjcl.codec.base64.fromBits(hash);
+                log.log(pbkdf2Opts);
+                http({
+                    method: 'POST',
+                    url: baseUrl + '/users',
+                    data: pbkdf2Opts
+                })
+                    .success(function(data, status, headers, config) {
+                        log.log(arguments);
+                    })
+                    .error(function(data, status, headers, config) {
+                        log.log(arguments);
+                    });
             };
         }
     ]);

4c420b954c8dfae33601a15dbbea784c123b01cf
Jesse Smick <janka102@gmail.com>
2014-07-07 17:34:13 -0700

    Added bongo.js

--- extension/js/background/background.js
+++ extension/js/background/background.js
@@ -1,1 +1,9 @@
 console.log('I\'m in the background!');
+
+bongo.db({
+    name: 'test',
+    objectStores: {
+        user: ['username', 'password', 'token'],
+        sites: ['domain', 'account']
+    }
+});
